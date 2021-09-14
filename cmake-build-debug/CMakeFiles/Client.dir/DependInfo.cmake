b4159c13648f651f557d2b79e17037e6c8b0561c
RoyAmoyal <roy15957@gmail.com>
2021-09-14 11:56:25 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/spl211/CLionProjects/SPL_Assignment3_Client/src/BGRSclient.cpp" "/home/spl211/CLionProjects/SPL_Assignment3_Client/cmake-build-debug/CMakeFiles/Client.dir/src/BGRSclient.cpp.o"
+  "/home/spl211/CLionProjects/SPL_Assignment3_Client/src/ClientToServer.cpp" "/home/spl211/CLionProjects/SPL_Assignment3_Client/cmake-build-debug/CMakeFiles/Client.dir/src/ClientToServer.cpp.o"
+  "/home/spl211/CLionProjects/SPL_Assignment3_Client/src/ConnectionHandler.cpp" "/home/spl211/CLionProjects/SPL_Assignment3_Client/cmake-build-debug/CMakeFiles/Client.dir/src/ConnectionHandler.cpp.o"
+  "/home/spl211/CLionProjects/SPL_Assignment3_Client/src/ServerToClient.cpp" "/home/spl211/CLionProjects/SPL_Assignment3_Client/cmake-build-debug/CMakeFiles/Client.dir/src/ServerToClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/src/BGRSclient.cpp.o"
+  "CMakeFiles/Client.dir/src/ClientToServer.cpp.o"
+  "CMakeFiles/Client.dir/src/ConnectionHandler.cpp.o"
+  "CMakeFiles/Client.dir/src/ServerToClient.cpp.o"
+  "Client"
+  "Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
